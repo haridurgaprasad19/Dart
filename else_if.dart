c9950654e41fa4e6ece3b8db71ffd91bde379fbe
haridurgaprasad19 <haridurgaprasad19@gmail.com>
2024-08-03 13:02:56 +0530

    else_if

---
+++ else_if.dart
@@ -0,0 +1,32 @@
+void main (){
+  var marks = 80;
+  if(marks==90){
+    print('A++');
+  }
+    else if(marks==80){
+      print('A');
+
+    }
+    else if(marks==70){
+      print('B');
+      
+    }else if(marks==60){
+      print('C');
+
+    }
+    else if(marks==50){
+      print('D');
+
+    }
+    else if (marks==35)
+    {
+      print('very poor');
+    }
+    else if(marks>=101||marks<=0){
+      print('invalid marks');
+    }
+    else{
+      print('fail');
+    }
+
+  }
